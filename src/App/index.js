00ddd374f0dec4d0ae96674ab7b5f6d2b3a1d014
Ritual <dev@theritual.eu>
2021-09-03 16:22:32 +0200

    created project and cleaned up

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+//fetch("https://api.exchangerate.host/latest").then( response => response.json()).then(jsonData => console.log(jsonData));
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        Created Project
+      </header>
+    </div>
+  );
+}
+
+export default App;
